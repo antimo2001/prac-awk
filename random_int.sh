ff9127fcd02b636abfc0c38c88764514c2757f09
antimo2001 <antimo2001@gmail.com>
2020-07-08 21:54:30 -0400

    add random_int.sh to generate random integers

---
+++ random_int.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MAXINT=${1:100}
+echo "...[random_int] Trace: MAXINT is $MAXINT"
+
+set -x
+awk -v MAXINT=$MAXINT 'BEGIN { srand() ; select = 1 + int(rand() * MAXINT) ; print select }'
+set +x
+
+echo "...[random_int] Trace: Done getting random integer up to $MAXINT"
