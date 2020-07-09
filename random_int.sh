ee5838e11c0c01765343a070df03af587b7fb69c
antimo2001 <antimo2001@gmail.com>
2020-07-08 22:20:13 -0400

    functionify for more generalization

--- random_int.sh
+++ random_int.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-MAXINT=${1:-100}
-echo "...[random_int] Trace: MAXINT is $MAXINT"
+function my-random-int {
+  MAXINT=$1
+  MAXINT=${MAXINT:-99}
+  # echo "...[random_int] Debug: MAXINT is $MAXINT"
 
-set -x
-awk -v MAXINT=$MAXINT 'BEGIN { srand() ; select = 1 + int(rand() * MAXINT) ; print select }'
-set +x
+  random=$(awk -v MAXINT=$MAXINT 'BEGIN { srand() ; select = 1 + int(rand() * MAXINT) ; print select }')
 
-echo "...[random_int] Trace: Done getting random integer up to $MAXINT"
+  # echo "...[random_int] Debug: Done getting random integer up to $MAXINT"
+  echo $random
+}
+
+my-random-int $1
